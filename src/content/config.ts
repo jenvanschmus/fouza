850fed12e1f296a926a61f507a963b857d6e1c48
jenvanschmus <jenvanschmus@gmail.com>
2025-12-30 17:12:26 -0800

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,14 @@
 import { defineCollection, z } from 'astro:content';
 
 const gallery = defineCollection({
-  type: 'content',
+  type: 'content', // 'content' type automatically handles the Markdown body
   schema: z.object({
     title: z.string(),
     image: z.string(),
-    date: z.date().optional(),
-    description: z.string().optional(), // We'll use this for the search
+    date: z.date().optional()
   }),
 });
 
-export const collections = { gallery };
+export const collections = {
+  'gallery': gallery,
+};
